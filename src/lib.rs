b53c278126755672bfda6e456da0949b718eb09d
kklibo <kklibo4@gmail.com>
2020-11-19 17:52:44 -0800

    added trivial doc comment referring to the README

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,13 @@
+//! A trivial Rust macro to derive the Display trait for any type with the Debug trait
+//!
+//! This is a very lightweight crate: check the README for more info
+
+
 
 extern crate proc_macro;
 
 use proc_macro::TokenStream;
 use quote::quote;
-
 
 #[proc_macro_derive(Display)]
 pub fn display_derive(input: TokenStream) -> TokenStream {
